862364f18e2583b085f3419bf6dbabc2fa18e196
Paolo <paolo.db@users.noreply.github.com>
2020-12-30 18:19:11 +0100

    Add Saving final model

--- implementation/road_signs_classification.py
+++ implementation/road_signs_classification.py
@@ -8,25 +8,22 @@ import matplotlib.pyplot as plt
 import keras
 from keras.utils import to_categorical
 from keras.models import Sequential
-from keras.layers import Conv2D,MaxPool2D,Dropout,Flatten,Dense
+from keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense
 from keras.layers import Conv2D, MaxPooling2D
 from keras.utils import to_categorical
 from keras.preprocessing import image
 from sklearn.model_selection import train_test_split
 
-
 tf.random.set_seed(100)
 
-path="..\\resources\\annotations"
-content=[]
+path = "..\\resources\\annotations"
+content = []
 speedcounter = 0
-
 
 for filename in os.listdir(path):
 
     if not filename.endswith('.xml'): continue
     finalpath = os.path.join(path, filename)
-
 
     infile = open(finalpath, "r")
 
@@ -69,37 +66,36 @@ new_cols = ["f_name", "path_name", "width", "height", "depth", "class_name"]
 data = pd.DataFrame(data=content, columns=new_cols)
 data.class_name = data.class_name.map({'trafficlight': 1, 'speedlimit': 2, 'crosswalk': 3, 'stop': 4})
 print(data.shape)
-#data.head()
+# data.head()
 
 print("Waiting. . .")
-data1=[]
+data1 = []
 
-i=0
+i = 0
 
 for a in data.path_name.values:
+    image = Image.open("..\\resources\\" + a).convert("RGB")
 
-    image = Image.open("..\\resources\\"+a).convert("RGB")
-
-    #Image resizing is needed to upgrade the resolution
-    image=image.resize((224,224),Image.ANTIALIAS)
-    image=np.array(image.getdata()).reshape(224,224,3)
+    # Image resizing is needed to upgrade the resolution
+    image = image.resize((224, 224), Image.ANTIALIAS)
+    image = np.array(image.getdata()).reshape(224, 224, 3)
     data1.append(image)
 
 print("---Done---")
 
-X=np.array(data1)
+X = np.array(data1)
 
-y=np.array(data.iloc[:,-1],dtype=int)
+y = np.array(data.iloc[:, -1], dtype=int)
 
-c=to_categorical(y,dtype=int)
-Y=c[:,1:]
+c = to_categorical(y, dtype=int)
+Y = c[:, 1:]
 
 X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=787)
 print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
 
-#Layer definition
+# Layer definition
 model = Sequential()
-model.add(Conv2D(128, kernel_size=(3 ,3), activation='relu', input_shape=X_train.shape[1:]))
+model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', input_shape=X_train.shape[1:]))
 model.add(MaxPool2D(pool_size=(2, 2)))
 model.add(Dropout(rate=0.25))
 model.add(Flatten())
@@ -107,15 +103,23 @@ model.add(Dense(128, activation='relu'))
 model.add(Dropout(rate=0.50))
 model.add(Dense(4, activation='softmax'))
 
-#Compilation of the model
-#categorical_crossentropy(multiclass classification problems)
-#Optimizer Adaptive Moment lr=0.001
+# Compilation of the model
+# categorical_crossentropy(multiclass classification problems)
+# Optimizer Adaptive Moment lr=0.001
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 
 history = model.fit(X_train, y_train, batch_size=8, epochs=50, validation_data=(X_test, y_test))
 
-#Evaluating
-results= model.evaluate(X_test,y_test,batch_size=8)
+# Evaluating
+results = model.evaluate(X_test, y_test, batch_size=8)
+
+#Saving final model
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+    json_file.write(model_json)
+# serialize weights to HDF5
+model.save_weights("model.h5")
+print("Saved model to drive")
 
 plt.plot(history.history['acc'])
 plt.plot(history.history['val_acc'])
